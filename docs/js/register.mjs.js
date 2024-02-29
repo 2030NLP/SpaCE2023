52715067d52311f196d68b6f5ffca658a3e60bf6
Lmxiao4 <86300641+Lmxiao4@users.noreply.github.com>
2024-02-29 21:15:49 +0800

    Update register.mjs.js

--- docs/js/register.mjs.js
+++ docs/js/register.mjs.js
@@ -16,7 +16,7 @@ const DEVELOPING_LOCAL = 0;
 const API_BASE_DEV_LOCAL = "http://127.0.0.1:5000";
 const DEV_HOSTS = ["http://192.168.124.5:8888", "http://192.168.1.100:8888", "http://10.1.108.200:8888/", "http://10.0.55.176:8888/", "http://10.1.124.56:8888/"];
 const API_BASE_DEV = DEV_HOSTS[0];
-const API_BASE_PROD = "https://www.lmxiao.cn";
+const API_BASE_PROD = "https://www.qinyuhang.cn";
 const API_BASE = DEVELOPING ? API_BASE_DEV : API_BASE_PROD;
 
 const BaseURL = `${API_BASE}/api/`;
